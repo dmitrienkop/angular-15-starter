c67471ef985dc451a32498c10068cb613cba4baf
Peter Dmitrienko <Petr.Dmitrienko@kaspersky.com>
2017-02-07 10:36:26 +0700

    heartbeat animation

--- src/app/training/training.controller.js
+++ src/app/training/training.controller.js
@@ -1,10 +1,13 @@
 class TrainingCtrl {
-    constructor() {
+    constructor($interval) {
         'ngInject';
 
         this.pulse = 134;
         this.photo = "http://gorabbit.ru/upload/uploader/23d/23dae1e5c7c29744e0a22766a13b170a.jpeg";
-    };
+
+        // TODO эмуляция получения данных с сервера, убрать
+        $interval(() => this.pulse = parseInt(Math.random() * (100 - 60) + 60, 10), 5000);
+    }
 }
 
 export default TrainingCtrl;
